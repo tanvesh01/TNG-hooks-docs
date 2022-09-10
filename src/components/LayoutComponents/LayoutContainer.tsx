76b2f77516896403e41bffb3b2310833bf16d4e3
tanvesh01 <sarvetanvesh01@gmail.com>
2022-09-11 04:16:48 +0530

    feat: added styles for sidebar

--- src/components/LayoutComponents/LayoutContainer.tsx
+++ src/components/LayoutComponents/LayoutContainer.tsx
@@ -1,13 +1,14 @@
 import { CSS, styled } from '@stitches/react';
-import React from 'react';
+import React, { useState } from 'react';
 
 const MainContainer = styled('main', {
   display: 'grid',
   gridTemplateColumns: '200px 2fr',
+  fontFamily: "'Inter', sans-serif",
 });
 
 const Sidebar = styled('aside', {
-  background: '#f9fcff',
+  background: '#151518',
   height: '100%',
 });
 
@@ -53,17 +54,85 @@ const MainContentContainer = styled('div', {
   margin: '15px',
   backgroundColor: 'White',
   borderRadius: '10px',
-  fontFamily: "'Inter', sans-serif",
   lineHeight: '28px',
   boxShadow:
     'rgb(50 50 93 / 14%) 0px 2px 11px 0px, rgb(0 0 0 / 20%) 0px 1px 3px -1px',
   ...styles,
 });
 
+const SidebarItemContainer = styled('ul', {
+  paddingLeft: '10px',
+
+  variants: {
+    childrenContainer: {
+      true: {
+        paddingLeft: '9px',
+        marginLeft: '13px',
+        borderLeft: '2px solid #e3e5eb',
+      },
+    },
+  },
+});
+
+const SidebarItem = styled('li', {
+  listStyle: 'none',
+  padding: '10px',
+  borderRadius: 5,
+  fontWeight: '600',
+  letterSpacing: '-0.02em',
+  color: ' #8b8c8e',
+
+  '&:hover': {
+    background: '#2b2c31',
+  },
+
+  variants: {
+    active: {
+      true: {
+        background: '#2b2c31',
+        paddingLeft: '20px',
+        color: 'white',
+      },
+    },
+    isHeading: {
+      true: {
+        fontSize: '1.2rem',
+        color: '#626262',
+        '&:hover': {
+          background: 'none',
+        },
+      },
+    },
+  },
+});
+
+const SidebarItemWithChildren = ({
+  parentLabel,
+  children,
+  isDefaultOpen = true,
+}) => {
+  return (
+    <div>
+      <SidebarItem isHeading>{parentLabel}</SidebarItem>
+      <SidebarItemContainer childrenContainer>{children}</SidebarItemContainer>
+    </div>
+  );
+};
+
 const LayoutContainer: React.FC<{ children: JSX.Element }> = ({ children }) => {
   return (
     <MainContainer>
-      <Sidebar />
+      <Sidebar>
+        <SidebarItemContainer>
+          <SidebarItem active>Introduction</SidebarItem>
+          <SidebarItemWithChildren parentLabel={'API'}>
+            <SidebarItem>useState</SidebarItem>
+            <SidebarItem>useReducer</SidebarItem>
+            <SidebarItem>useCallback</SidebarItem>
+            <SidebarItem>useEffect</SidebarItem>
+          </SidebarItemWithChildren>
+        </SidebarItemContainer>
+      </Sidebar>
       <MainContentContainer>{children}</MainContentContainer>
     </MainContainer>
   );
